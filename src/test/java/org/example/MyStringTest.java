3c03640d6673329757495f264a5dc85a849599e7
Saeed-Moradi-1998 <saeed.moradi.edu@gmail.com>
2024-03-27 20:35:00 -0500

    Red. The second test case fails as it checks if s1 is empty or not. The expected value is -1 but it returns -2.

--- src/test/java/org/example/MyStringTest.java
+++ src/test/java/org/example/MyStringTest.java
@@ -10,5 +10,6 @@ public class MyStringTest {
         // These are the initial test cases of method named indexOfString.
         // If either of the strings are blank, the output should be -1.
         assertEquals(-1, MyString.indexOfString("world", "", 0));
+        assertEquals(-1, MyString.indexOfString("", "world", 0));
     }
 }
