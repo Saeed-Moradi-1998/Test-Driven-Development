7ce54ee159d51c0723ee3dcd5c14a5f248fbea09
Saeed-Moradi-1998 <saeed.moradi.edu@gmail.com>
2024-03-27 20:30:15 -0500

    Green. The initial test now successfully passes for the method indexOfString when s2 is empty.

--- src/main/java/org/example/MyString.java
+++ src/main/java/org/example/MyString.java
@@ -3,6 +3,11 @@ package org.example;
 public class MyString {
 
     public static int indexOfString(String s1, String s2, int pos) {
+
+        if (s2.isEmpty()){
+            return -1; // This condition checks if s2 is empty or not.
+        }
+
         return -2; // This is the initial implementation of this function which has a blank body and return a wrong number.
     }
 }
