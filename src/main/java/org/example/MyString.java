5a82988e445433029c5be20e227b85dd6bdb42ed
Saeed-Moradi-1998 <saeed.moradi.edu@gmail.com>
2024-03-27 20:37:34 -0500

    Refactor. The if conditions on the method indexOfString are merged. They return -1 if either s1 or s2 are empty strings.

--- src/main/java/org/example/MyString.java
+++ src/main/java/org/example/MyString.java
@@ -4,12 +4,8 @@ public class MyString {
 
     public static int indexOfString(String s1, String s2, int pos) {
 
-        if (s2.isEmpty()){
-            return -1; // This condition checks if s2 is empty or not.
-        }
-
-        if (s1.isEmpty()){
-            return -1;
+        if (s1.isEmpty() || s2.isEmpty()){
+            return -1; // This condition checks if either of s1 and s2 are empty or not.
         }
 
         return -2; // This is the initial implementation of this function which has a blank body and return a wrong number.
