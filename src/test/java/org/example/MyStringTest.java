62691fce4e910759cd6ef346648631bcce784e13
Saeed-Moradi-1998 <saeed.moradi.edu@gmail.com>
2024-03-27 20:40:03 -0500

    Red. The third test of method named indexOfString has failed. It checks if the position of s2 in s1 is correctly found starting from the beginning of s1.

--- src/test/java/org/example/MyStringTest.java
+++ src/test/java/org/example/MyStringTest.java
@@ -11,5 +11,7 @@ public class MyStringTest {
         // If either of the strings are blank, the output should be -1.
         assertEquals(-1, MyString.indexOfString("world", "", 0));
         assertEquals(-1, MyString.indexOfString("", "world", 0));
+        // This test checks if s2 can be found in s1.
+        assertEquals(6, MyString.indexOfString("hello world", "world", 0));
     }
 }
