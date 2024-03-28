9dace52d1ea3f7241702ddc54ff952b1ac4dda6b
Saeed-Moradi-1998 <saeed.moradi.edu@gmail.com>
2024-03-27 20:47:38 -0500

    Green. The third test case for the method indexOfString passes. It checks the starting index of s2 in s1 is correctly found and returned.

--- src/main/java/org/example/MyString.java
+++ src/main/java/org/example/MyString.java
@@ -3,11 +3,29 @@ package org.example;
 public class MyString {
 
     public static int indexOfString(String s1, String s2, int pos) {
-
-        if (s1.isEmpty() || s2.isEmpty()){
-            return -1; // This condition checks if either of s1 and s2 are empty or not.
+        // Check if either s1 or s2 is null or empty
+        if (s1.isEmpty() || s2.isEmpty()) {
+            return -1;
         }
 
-        return -2; // This is the initial implementation of this function which has a blank body and return a wrong number.
+        // Iterate through s1 starting from pos
+        for (int i = pos; i <= s1.length() - s2.length(); i++) {
+            // Check if s2 matches the substring of s1 starting at i
+            boolean found = true;
+            for (int j = 0; j < s2.length(); j++) { // Iterating through all the characters of s2
+                if (s1.charAt(i + j) != s2.charAt(j)) {
+                    found = false; // If there is a mismatch between any of the characters of s1 and s2,
+                    // we can jump to the next set of characters.
+                    break;
+                }
+            }
+            // If s2 is found starting at index i, return i
+            if (found) {
+                return i;
+            }
+        }
+
+        // If s2 is not found in s1, return -1
+        return -1;
     }
 }
